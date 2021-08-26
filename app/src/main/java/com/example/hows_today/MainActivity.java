bb198e995771a1f1acdff20507e2e25ac299ff3b
lkjjjang <lkjjjang@naver.com>
2021-08-26 18:20:25 +0900

    maicactivity modify

--- app/src/main/java/com/example/hows_today/MainActivity.java
+++ app/src/main/java/com/example/hows_today/MainActivity.java
@@ -92,8 +92,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         // 위치 권한 설정
         if (!checkLocationServicesStatus()) {
             showDialogForLocationServiceSetting();
